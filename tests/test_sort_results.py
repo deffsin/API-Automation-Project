a7b5679b0af1ba3242c7731be952498949018bb7
deffsin <deffsinco@gmail.com>
2025-01-03 17:59:11 +0200

    Fix: improved test for sorting API results

--- tests/test_sort_results.py
+++ tests/test_sort_results.py
@@ -1,19 +1,24 @@
 import pytest
 from helpers.api_client import APIClient
-from config.logging_config import logger
 from helpers.api_helpers import get_sort_results
+from helpers.api_helpers import get_products
+from config.logging_config import logger
 
 @pytest.fixture
 def api_client():
     return APIClient()
 
 def test_get_sort_results(api_client):
-    products = get_sort_results(api_client, value="asc")
+    order = "asc" # or 'asc'
 
-    assert isinstance(products, list), "Response should be a list"
-    assert len(products) > 0, "Products list is empty"
+    all_products = sorted(get_products(api_client), key=lambda x: x["id"], reverse=False) # if 'desc' then True
+    sorted_products = get_sort_results(api_client, value=order)
 
-    for product in products:
+    assert isinstance(sorted_products, list), "Response should be a list"
+    assert len(sorted_products) > 0, "Products list is empty"
+    assert all_products == sorted_products, "Products are not sorted correctly"
+
+    for product in sorted_products:
         assert "id" in product, "Product ID is missing"
         assert "title" in product, "Product title is missing"
         assert "price" in product, "Product price is missing"
@@ -21,4 +26,4 @@ def test_get_sort_results(api_client):
         assert "description" in product, "Product description is missing"
         assert "image" in product, "Product image is missing"
 
-    logger.info(f"Product data: {products}")
+    logger.info(f"Product data: {sorted_products}")
